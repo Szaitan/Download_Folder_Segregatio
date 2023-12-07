84fbb7f0ab0259ea45217179106e9065deca78b8
Michał <m.krepiniewicz@gmail.com>
2023-12-07 23:30:51 +0100

    Rev 1.1 - Dynamic direction finding

--- main.py
+++ main.py
@@ -12,37 +12,30 @@ for fname in os.listdir(downloads_path):
     path = os.path.join(downloads_path, fname)
 
     if os.path.isdir(path):
-        continue
+        pass
     else:
         split_file = fname.rsplit(".", 1)
-        list_with_file_type.append(split_file[1])
+        if split_file[1] in list_with_file_type:
+            pass
+        else:
+            list_with_file_type.append(split_file[1])
         list_with_files_to_move.append(fname)
 
-print(list_with_file_type)
-print(list_with_files_to_move)
-
-list_with_file_type_no_duplicates = list(dict.fromkeys(list_with_file_type))
-
-if os.path.isdir(f"../../../{os.getlogin()}/Desktop/Segregated Files"):
+if os.path.isdir(f"{downloads_path}/Segregated Files"):
     print(True)
 else:
-    os.mkdir(os.path.join(f"../../../{os.getlogin()}/Desktop", "Segregated Files"))
+    os.mkdir(os.path.join(f"{downloads_path}", "Segregated Files"))
 
+for file_type in list_with_file_type:
+    if not os.path.isdir(f"{downloads_path}/Segregated Files/{file_type}"):
+        os.mkdir(os.path.join(f"{downloads_path}/Segregated Files", file_type))
 
-for file_type in list_with_file_type_no_duplicates:
-    if not os.path.isdir(f"../../../{os.getlogin()}/Desktop/Segregated Files/{file_type}"):
-        os.mkdir(os.path.join(f"../../../{os.getlogin()}/Desktop/Segregated Files", file_type))
-
-print(list_with_files_to_move)
-print(list_with_file_type_no_duplicates)
-
-for file_ending in list_with_file_type_no_duplicates:
+for file_ending in list_with_file_type:
     for file in list_with_files_to_move:
         split_file = file.rsplit(".", 1)
-        print(file)
-        print(split_file[1])
+
         if file_ending == split_file[1]:
-            print('test')
+
             src_path = os.path.join(downloads_path, file)
-            dst_path = os.path.join(f"../../../{os.getlogin()}/Desktop/Segregated Files/{file_ending}", file)
+            dst_path = os.path.join(f"{downloads_path}/Segregated Files/{file_ending}", file)
             os.rename(src_path, dst_path)
